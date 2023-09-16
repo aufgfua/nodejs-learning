a50205a68611eca711b22ef09cb6e6531f14a1b0
Augusto Flach <augustofalcaoflach@gmail.com>
2023-09-16 22:48:19 +0100

    update(structure): create hbs templates

--- 3-webserver-project/src/app.js
+++ 3-webserver-project/src/app.js
@@ -1,29 +1,50 @@
 const path = require("path");
 
 const express = require("express");
+const hbs = require("hbs");
 
 const publicFolder = path.join(__dirname, "../public");
+const templateFolder = path.join(__dirname, "../templates");
+const viewsFolder = path.join(templateFolder, "views");
+const partialsFolder = path.join(templateFolder, "partials");
+
 const app = express();
 
 app.set("view engine", "hbs");
+app.set("views", viewsFolder);
+hbs.registerPartials(partialsFolder);
+
 app.use(express.static(publicFolder));
 
-app.get("/", (req, resp) => {
-    resp.render("index", { title: "HBS Dynamic Title" });
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Base Page",
+        content: "Base Page Content",
+        footer: "Footer content",
+    });
 });
 
-app.get("/help", (req, resp) => {
-    resp.send("help");
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help page",
+        content: "Helpful Content",
+        footer: "Footer content",
+    });
 });
 
-app.get("/about", (req, resp) => {
-    resp.send("<h1>about</h1>");
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "Help 404",
+        content: "Help content not found",
+        footer: "Help Page Accessed Footer",
+    });
 });
 
-app.get("/weather", (req, resp) => {
-    resp.send({
-        name: "name",
-        old: "old",
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        content: "Generic content not found",
+        footer: "Generic Page Accessed Footer",
     });
 });
 
